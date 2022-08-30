6135518675951eef8e5f6f60e52cac562679828d
JJiWoNing <jjiwoning12@gmail.com>
2022-08-30 21:23:36 +0900

    String charAt Test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -23,6 +24,16 @@ public class StringTest {
         String target = "(1,2)";
         String result = target.substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 함수 학습 테스트")
+    void charAt(){
+        String target = "abc";
+        char result = target.charAt(0);
+        assertThat(result).isEqualTo('a');
+        assertThatThrownBy(() -> target.charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 
 }
