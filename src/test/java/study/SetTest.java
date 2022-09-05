06708d8409ff786c9512adbfd9425c07447e31fc
JJiWoNing <jjiwoning12@gmail.com>
2022-09-05 18:17:43 +0900

    set contains test use CsvSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int input){
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, true", "2, true", "3, true", "4, false", "5, false"})
+    void containsInFalse(int input, boolean expected){
+        boolean result = numbers.contains(input);
+        assertThat(result).isEqualTo(expected);
     }
 }
